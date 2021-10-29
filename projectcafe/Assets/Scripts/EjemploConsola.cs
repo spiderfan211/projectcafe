68bc63b57cdb8aaaa120b947cdc157384a5ef33c
spiderfan211 <spiderfan211@gmail.com>
2021-10-29 11:21:04 +0200

    actualización periodica

---
+++ projectcafe/Assets/Scripts/EjemploConsola.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EjemploConsola : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Mensaje de información");
+        Debug.LogWarning("oh nooo");
+        Debug.LogError("OH NOOO");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
